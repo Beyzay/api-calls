719ed8837b14ebe11777246d611554637bf0f527
untitled <62313077+Beyzay@users.noreply.github.com>
2025-08-11 23:55:21 -0400

    Add input form

--- js/script.js
+++ js/script.js
@@ -1,5 +1,12 @@
 // Assign DOM Elements to variables
 const fetchGetBtn = document.getElementById("fetch-get-btn");
 const xhrGetBtn = document.getElementById("xhr-get-btn");
+const form = document.getElementById("form");
+const formMethodSelect = document.getElementById("form-method-select");
+const postId = document.getElementById("post-id");
+const postTitle = document.getElementById("post-title");
+const postContent = document.getElementById("post-content");
 const fetchedData = document.getElementById("fetched-data");
 const errMsg = document.getElementById("err-msg");
+const submitBtn = document.getElementById("form-submit-btn");
+
