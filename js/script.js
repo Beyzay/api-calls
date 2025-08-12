c321c368e454e95a21edb7afff03783b64609e10
untitled <62313077+Beyzay@users.noreply.github.com>
2025-08-11 23:54:22 -0400

    Add fetched data and error message elements

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
 // Assign DOM Elements to variables
 const fetchGetBtn = document.getElementById("fetch-get-btn");
 const xhrGetBtn = document.getElementById("xhr-get-btn");
+const fetchedData = document.getElementById("fetched-data");
+const errMsg = document.getElementById("err-msg");
